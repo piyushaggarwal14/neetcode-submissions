f9e44d66de080fd510edb538ec723ed742bdd3a6
Piyush Aggarwal <57876036+piyushaggarwal14@users.noreply.github.com>
2026-07-22 14:07:03 +0530

    Add: majority-element - submission-0

---
+++ Data Structures & Algorithms/majority-element/submission-0.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int n = nums.size();
+        unordered_map<int,int> count;
+        for(int i : nums){
+            count[i]++;
+            if(count[i]>(n/2)){
+                return i;
+            }
+        }
+    }
+};
